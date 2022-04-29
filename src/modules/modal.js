79a4238384bb209498625ba51228c0367b41a29b
VanyaKornev2089 <94173875+VanyaKornev2089@users.noreply.github.com>
2022-04-29 13:50:42 +0300

    Add files via upload

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,6 +1,7 @@
 const modal = () => {
     const bacground = document.querySelector('.overlay')
     const modalContent = document.querySelector('.header-modal')
+    const twoModal = document.querySelector('.services-modal')
 
 
     document.addEventListener('click', (e) => {
@@ -14,7 +15,24 @@ const modal = () => {
             bacground.style.display = ""
             modalContent.style.display = ""
         }
+
+        if (e.target.closest('.utp-button > .fancyboxModal')) {
+            bacground.style.display = "block"
+            twoModal.style.display = "block"
+        }
+
+        if (e.target.closest('.service-button > .fancyboxModal')) {
+            bacground.style.display = "block"
+            twoModal.style.display = "block"
+        } else if (e.target.closest('.services-modal__close')) {
+            bacground.style.display = ""
+            twoModal.style.display = ""
+        } else if (e.target.closest('.overlay')) {
+            bacground.style.display = ""
+            twoModal.style.display = ""
+        }
     })
+
 }
 
 export default modal

05db42264b38d2dd790279c4d306efba258abc1d
VanyaKornev2089 <94173875+VanyaKornev2089@users.noreply.github.com>
2022-05-05 13:14:54 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import twoSlider from './modules/twoSlider'
 import scroll from './modules/scroll'
 import timer from './modules/timer'
 import calculator from './modules/calculator'
+import galery from './modules/galery'
 
 modal()
 slider()
@@ -11,3 +12,4 @@ scroll()
 timer('25 july 2022')
 calculator()
 twoSlider()
+galery()
